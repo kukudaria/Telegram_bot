8d457e71a3b06a55b810f439e5ac3dc870152670
zaxon02 <zaxon02@gmail.com>
2021-04-17 21:28:25 +0300

    ъуъ

--- dtest.py
+++ dtest.py
@@ -3,14 +3,14 @@ from sqlalchemy import *
 engine = create_engine("sqlite:///dbfile.sqlite")
 connection = engine.connect()
 metadata = MetaData()
-cities_ = ["Paris","London","Saint Petersburg","Barcelona","Berlin","Madrid",
-           "Kyiv","Birmingham","Rome","Manchester","Minsk","Bucharest","Vienna",
-           "Hamburg","Warsaw","Budapest","Newcastle","Munich","Belgrade","Milan",
-           "Sofia","Prague","Sevilla","Dublin","Copenhagen","Cologne","Amsterdam",
-           "Odesa","Rotterdam","Stockholm","Zagreb","Riga","Oslo","Athens","Helsinki",
-           "Skopje","Dnipro","Glasgow","Naples","Turin","Marseille","Liverpool","Portsmouth",
-           "Valencia","Nottingham","Krakow","Frankfurt","Bristol","Lviv","Bremen","Grenoble",
-           "Lodz","Sheffield","Palermo","Zaragoza","Wroclaw","Nantes","Stuttgart","Dusseldorf","Gothenburg"]
+cities_ = ["Paris", "London", "Saint Petersburg", "Barcelona", "Berlin", "Madrid",
+           "Kyiv", "Birmingham", "Rome", "Manchester", "Minsk", "Bucharest", "Vienna",
+           "Hamburg", "Warsaw", "Budapest", "Newcastle", "Munich", "Belgrade", "Milan",
+           "Sofia", "Prague", "Sevilla", "Dublin", "Copenhagen", "Cologne", "Amsterdam",
+           "Odesa", "Rotterdam", "Stockholm", "Zagreb", "Riga", "Oslo", "Athens", "Helsinki",
+           "Skopje", "Dnipro", "Glasgow", "Naples", "Turin", "Marseille", "Liverpool", "Portsmouth",
+           "Valencia", "Nottingham", "Krakow", "Frankfurt", "Bristol", "Lviv", "Bremen", "Grenoble",
+           "Lodz", "Sheffield", "Palermo", "Zaragoza", "Wroclaw", "Nantes", "Stuttgart", "Dusseldorf", "Gothenburg"]
 cities = Table('cities', metadata,
         Column('city_id', Integer, primary_key=True),
         Column('city', String(16), nullable=False)
