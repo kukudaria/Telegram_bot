711a9598625e79694e47371e8f789d2a680b7ae5
zaxon02 <zaxon02@gmail.com>
2021-04-24 20:31:41 +0300

    ver 1.0 is finished

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,9 +19,9 @@ class BotWrapper:
         self.logger = logging.getLogger(__name__)
         self.updater = Updater(token=token, use_context=True)
         self.dispatcher = self.updater.dispatcher
-        self.dispatcher.add_handler(MessageHandler(Filters.text, self.echo))
+        self.dispatcher.add_handler(MessageHandler(Filters.text, self.send_message))
         self.dispatcher.add_handler(CallbackQueryHandler(self.callback_message))
-       # self.dispatcher.add_error_handler(self.catch_exeptions)
+        self.dispatcher.add_error_handler(self.catch_exeptions)
         self.engine = create_engine(path_to_db, connect_args={'check_same_thread': False})
         self.connection = self.engine.connect()
         self.metadata = MetaData()
@@ -38,18 +38,23 @@ class BotWrapper:
         self.result = self.connection.execute(select(self.cities.c.city)).all()
 
     def catch_exeptions(self, update, context):
-        context.bot.send_message(update.message.chat.id, self.logger.warning('Update "%s" caused error "%s"', update, context.error))
+        self.logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-    def echo(self, update, context):
-        self.marks(update, context)
-        self.create_reply_button()
-        context.bot.send_message(update.message.chat.id, self.message, reply_markup=self.reply_keyboard)
-        context.bot.send_photo(update.message.chat.id, self.response.content, reply_markup=self.keyboard)
+    def send_message(self, update, context):
         user_id = update.message.from_user.id
         id_ = self.connection.execute(select(self.statistics.c.user_id)).all()
         id = [item for t in id_ for item in t]
         if not user_id in id:
-            self.connection.execute(self.statistics.insert(), {"user_id": user_id, 'right_answers': 0, 'wrong_answers': 0, 'stamp': ''})
+            self.connection.execute(self.statistics.insert(),
+                                    {"user_id": user_id, 'right_answers': 0, 'wrong_answers': 0, 'stamp': ''})
+
+        if update.message.text == 'Reset statistics':
+            self.zero_statistics(update, context)
+        else:
+            self.marks(update, context)
+            self.create_reply_button()
+            context.bot.send_message(update.message.chat.id, self.message, reply_markup=self.reply_keyboard)
+            context.bot.send_photo(update.message.chat.id, self.response.content, reply_markup=self.keyboard)
 
     def marks(self, update, context):
         long_list = []
@@ -62,16 +67,19 @@ class BotWrapper:
             lat_list.append(location.latitude)
         map_request = 'http://static-maps.yandex.ru/1.x/?l=sat&pt='
         for i in range(3):
-            map_request += str(long_list[i]) + ',' + str(lat_list[i]) + ',pm2rdm' + str(i + 1) + '~'
-        map_request += str(long_list[3]) + ',' + str(lat_list[3]) + ',pm2rdm' + '4'
+            map_request += str(long_list[i]) + ',' + str(lat_list[i]) + ',pm2rdl' + str(i + 1) + '~'
+        map_request += str(long_list[3]) + ',' + str(lat_list[3]) + ',pm2rdl' + '4'
         self.response = requests.get(map_request)
 
     def create_reply_button(self):
         button = KeyboardButton('Reset statistics')
-        self.reply_keyboard = ReplyKeyboardMarkup([[button]], one_time_keyboard=False)
+        self.reply_keyboard = ReplyKeyboardMarkup([[button]], one_time_keyboard=False, resize_keyboard=True)
 
-    #def zero_stastistics(self, update, context):
-
+    def zero_statistics(self, update, context):
+        user_id = update.message.from_user.id
+        self.connection.execute(self.statistics.update().where(self.statistics.c.user_id == user_id),
+                                {'right_answers': 0, 'wrong_answers': 0})
+        context.bot.send_message(update.message.chat.id, "Your statistics is reseted")
 
     def create_message(self, update, context):
         now = datetime.now()
@@ -94,7 +102,7 @@ class BotWrapper:
             tag = str(i + 1)
             button = InlineKeyboardButton(tag, callback_data=self.out[i] + ',' + self.asked_city + ',' + right_answer + ',' + timestamp)
             self.button_list.append(button)
-        self.message = 'What number is ' + str(self.asked_city) + '?'
+        self.message = 'Which number is ' + str(self.asked_city) + '?'
         self.keyboard = InlineKeyboardMarkup([self.button_list])
 
     def callback_message(self, update, context):
@@ -118,7 +126,7 @@ class BotWrapper:
                         print(i, j)
                         statistic = int((i + 1) / (i + j + 1) * 100)
                         print(statistic)
-                context.bot.send_message(update.callback_query.message.chat.id, f'Great! Your score is {statistic}% of right answers')
+                context.bot.send_message(update.callback_query.message.chat.id, f'Great!\n{statistic}% of your answers are correct.')
 
             else:
                 for _ in callback_data:
@@ -127,7 +135,7 @@ class BotWrapper:
                             self.connection.execute(self.statistics.update().where(self.statistics.c.user_id == user_id), {'wrong_answers': int(i) + 1})
                             for j in right:
                                 statistic = int(j / (i + j + 1) * 100)
-                        context.bot.send_message(update.callback_query.message.chat.id, f"Wrong! Right answer was {callback_data[2]}. Your score is {statistic}% of right answers")
+                        context.bot.send_message(update.callback_query.message.chat.id, f"Wrong! Right answer was {callback_data[2]}.\n{statistic}% of your answers are correct.")
                         break
             self.create_message(update, context)
             self.marks(update, context)
@@ -136,7 +144,7 @@ class BotWrapper:
                                    reply_markup=self.keyboard)
         else:
             context.bot.send_message(update.callback_query.message.chat.id,
-                                     'You have already answered this question))))))))')
+                                     'You have already answered this question:))))))))')
 
         update.callback_query.answer()
 
--- working_progress/test.py
+++ working_progress/test.py
@@ -12,9 +12,12 @@ cities_ = ["Paris", "London", "Saint Petersburg", "Barcelona", "Berlin", "Madrid
            "Skopje", "Dnipro", "Glasgow", "Naples", "Turin", "Marseille", "Liverpool", "Portsmouth",
            "Valencia", "Nottingham", "Krakow", "Frankfurt", "Bristol", "Lviv", "Bremen", "Grenoble",
            "Lodz", "Sheffield", "Palermo", "Zaragoza", "Wroclaw", "Nantes", "Stuttgart", "Dusseldorf", "Gothenburg"]
-cities = Table('cities', metadata,
-        Column('city_id', Integer, primary_key=True),
-        Column('city', String(16), nullable=False)
-    )
-for i in range(len(cities_)):
-    connection.execute(cities.insert(), {"city_id": i, "city": str(cities_[i])})
+statistics = Table('statistics', metadata,
+                               Column('user_id', Integer, primary_key=True),
+                               Column('right_answers', Integer),
+                               Column('wrong_answers', Integer)
+                       )
+
+
+
+engine.execute('alter table statistics add column stamp')
